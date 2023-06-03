c9912ea4b5af7e100f0a3adb3694b5210f9df1eb
Patrick Nazareth <86360051+Nazareth98@users.noreply.github.com>
2023-06-02 23:12:17 -0300

    att

--- src/Components/OthesProjects/cards/cardsItems.jsx
+++ src/Components/OthesProjects/cards/cardsItems.jsx
@@ -1,60 +1,14 @@
-const Items = [
-    {
-        id: '01',
-        title: 'Pokedex',
-        description: 'Consumo de API REST utilizando boas práticas.',
-        github: 'https://github.com/Nazareth98/Pokedex-PokeAPI',
-        url: '#'
-    },
-    {
-        id: '02',
-        title: 'Controle Financeiro',
-        description: 'Desenvolvido com JavaScript Vanilla.',
-        github: 'https://github.com/Nazareth98/ControleFinanceiro-JSVanilla',
-        url: '#'
-    },
-    {
-        id: '03',
-        title: 'Urna Eletrônica',
-        description: 'Aprimorando lógica de programação e algorítimo.',
-        github: 'https://github.com/Nazareth98/Urna-Eletronica',
-        url: '#'
-    },
-    {
-        id: '04',
-        title: 'Ledosh',
-        description: 'Landing page utilizando apenas HTML5 e CSS3.',
-        github: 'https://github.com/Nazareth98/ledosh',
-        url: '#'
-    },
-    {
-        id: '05',
-        title: 'ZordStore',
-        description: 'Reimaginação do site da mninha antiga marca de roupa.',
-        github: 'https://github.com/Nazareth98/ZordStore',
-        url: '#'
-    },
-    {
-        id: '06',
-        title: 'Calculadora Iphone',
-        description: 'Inspirada no design atual da calculadora da Apple',
-        github: 'https://github.com/Nazareth98/Iphone-Calculator',
-        url: '#'
-    },
-    {
-        id: '07',
-        title: 'Calculadora de IMC',
-        description: 'Após inserir alguns dados ele nos retorna seu IMC.',
-        github: 'https://github.com/Nazareth98/Calculadora-de-IMC',
-        url: '#'
-    },
-    {
-        id: '08',
-        title: 'Churrascômetro',
-        description: 'Projeto realizado em paralelo ao curso ProgamadorBR.',
-        github: 'https://github.com/Nazareth98/ProgramadorBrChurrascometro',
-        url: '#'
-    }
-]
+const getRepos = async () => {
+  try {
+    const response = await fetch(
+      "https://api.github.com/users/Nazareth98/repos"
+    );
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export default Items
+export default getRepos;
--- src/Components/OthesProjects/index.jsx
+++ src/Components/OthesProjects/index.jsx
@@ -1,30 +1,27 @@
-import React from 'react'
-import '../../Styles/Components/otherProject.scss'
-import SectionHeader from '../Components/SectionHeader/index'
-import Card from './cards/index'
-
+import React from "react";
+import "../../Styles/Components/otherProject.scss";
+import SectionHeader from "../Components/SectionHeader/index";
+import Card from "./cards/index";
 
 const OtherProjects = () => {
-    const [seeMore, setSeeMore] = React.useState(false)
+  const [seeMore, setSeeMore] = React.useState(false);
 
-    return (
+  return (
+    <>
+      <article className="container cards-container">
+        <SectionHeader id="+ " title="Outros Projetos" />
 
-        <>
-            <article className='container cards-container'>
-                <SectionHeader id="+ " title='Outros Projetos' />
+        <div className="cards-box">
+          <Card seeMore={seeMore} />
+        </div>
+        <div className="button-box" onClick={() => setSeeMore(!seeMore)}>
+          <button data-aos="fade-up" data-aos-duration="1200">
+            {seeMore ? "Ver Menos" : "Ver Mais"}
+          </button>
+        </div>
+      </article>
+    </>
+  );
+};
 
-                <div className='cards-box'>
-                    <Card seeMore={seeMore} />
-                </div>
-                <div className='button-box' onClick={() => setSeeMore(!seeMore)}>
-                    <button
-                        data-aos="fade-up"
-                        data-aos-duration="1200"
-                    >{seeMore ? 'Ver Menos' : 'Ver Mais'}</button>
-                </div>
-            </article>
-        </>
-    )
-}
-
-export default OtherProjects
+export default OtherProjects;
